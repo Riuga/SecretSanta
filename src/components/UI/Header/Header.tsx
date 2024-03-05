b55361c0a97a9559020bc66030ea8318ad53dd33
riuga <mosks@bk.ru>
2024-03-05 12:50:58 +0300

    update: header

--- src/components/UI/Header/Header.tsx
+++ src/components/UI/Header/Header.tsx
@@ -1,4 +1,5 @@
-import { signout } from '../../../api/AuthService'
+import { useEffect, useState } from 'react'
+import { fetchSelfData, signout } from '../../../api/AuthService'
 import { HomeIcon } from '../../../assets/HomeIcon'
 import { LogoutIcon } from '../../../assets/LogoutIcon'
 import { HeaderProps } from '../../../utils/types'
@@ -6,13 +7,23 @@ import './Header.css'
 import { Link } from 'react-router-dom'
 
 function Header(props: HeaderProps) {
+  const [username, setUsername] = useState('')
+
+  useEffect(() => {
+    fetchSelfData()
+      .then((data) => {
+        setUsername(data.username)
+      })
+      .catch(console.log)
+  })
+
   return (
     <header>
       <Link to='/list'>
         <HomeIcon />
       </Link>
       <h1>{props.title}</h1>
-      <p>User: {props.username}</p>
+      <p>User: {username}</p>
       <button style={{ background: 'none' }} onClick={() => signout()}>
         <LogoutIcon />
       </button>
