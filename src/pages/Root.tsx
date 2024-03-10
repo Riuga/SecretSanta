5f612db3764f5e47414aa0100b8d468b2c6fb2a5
riuga <mosks@bk.ru>
2024-03-11 02:58:31 +0300

    add: root page

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,5 +1,15 @@
+import { Link } from 'react-router-dom'
+import '../styles/Root.css'
+
 function Root() {
-  return <div></div>
+  return (
+    <div className='root-page'>
+      <h1 className='red'>Welcome!</h1>
+      <h2>Merry Christmas and Happy New Year!</h2>
+      <p>Wanna play Secret Santa with your friends?</p>
+      <Link to='/signup'>Join</Link>
+    </div>
+  )
 }
 
 export default Root
